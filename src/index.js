900a374722bb2ee555cce8c6ca3a04770f92af34
Zain Adam <zainadam.id@gmail.com>
2022-07-05 19:58:05 +0700

    refactor: move connectThirdParty -> reportWebVital

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
+import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import connectThirdParty from './helpers/connectThirdParty';
 
-if (process.env.NODE_ENV === 'production') {
-	// analytic and error reporting
-	connectThirdParty()
-}
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -17,13 +12,8 @@ root.render(
 	</React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();
-serviceWorkerRegistration.register();
+if (process.env.NODE_ENV === 'production') {
+	reportWebVitals();
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorkerRegistration.register();
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,56 @@
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const reportWebVitals = () => {
+	Sentry.init({
+		dsn: process.env.REACT_APP_SENTRY_DSN,
+		integrations: [new BrowserTracing()],
+		tracesSampleRate: process.env.REACT_APP_SENTRY_TRACE_SAMPLE_RATE,
+	});
+
+	const firebaseConfig = {
+		apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+		authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+		storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.REACT_APP_FIREBASE_APP_ID,
+		measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+	};
+
+	const app = initializeApp(firebaseConfig);
+	// const ga = getAnalytics(app);
+	getAnalytics(app);
+
+	// console.log(ga.sen);
+
+	// const sendToAnalytics = ({ id, name, value }) => {
+	// 	logEvent('send', 'event', {
+	// 		eventCategory: 'Web Vitals',
+	// 		eventAction: name,
+	// 		eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+	// 		eventLabel: id, // id unique to current page load
+	// 		nonInteraction: true, // avoids affecting bounce rate
+	// 	});
+	// }
+
+	// reportWebVitalsDefault(sendToAnalytics)
+}
+
+// defult report web vital
+// const reportWebVitalsDefault = (onPerfEntry) => {
+// 	if (onPerfEntry && onPerfEntry instanceof Function) {
+// 		import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+// 			getCLS(onPerfEntry);
+// 			getFID(onPerfEntry);
+// 			getFCP(onPerfEntry);
+// 			getLCP(onPerfEntry);
+// 			getTTFB(onPerfEntry);
+// 		});
+// 	}
+// };
 
 export default reportWebVitals;
