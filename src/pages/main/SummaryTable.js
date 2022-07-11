9375d61d374430eb06c158ba31b59ecbf732efb3
Zain Adam <zainadam.id@gmail.com>
2022-07-11 13:40:27 +0700

    chore: refactor SummaryTable

--- src/pages/main/SummaryTable.js
+++ src/pages/main/SummaryTable.js
@@ -8,33 +8,26 @@ import TableRow from '@mui/material/TableRow';
 import CurrencyFormattedCell from '../../components/CurrencyFormattedCell';
 import DefaultFormattedCell from '../../components/DefaultFormattedCell';
 
+const SX_TABLE_CONTAINER = { mt: 2 }
+const SX_TABLE = { 'th': { fontWeight: 'bold' }, 'td, th': { whiteSpace: 'nowrap' } }
 
-export default function SummaryTable({dataset}) {
-	// const [dataset] = useState(props.dataset || [])
+const Rows = ({ data }) =>
+	<TableRow>
+		<TableCell component={data.name === 'PENDAPATAN' ? 'th' : 'td'} scope="row">
+			{data.name}
+		</TableCell>
+		<DefaultFormattedCell value={data.weight} unit='kg' th={data.name === 'PENDAPATAN'} />
+		<CurrencyFormattedCell th={data.name === 'PENDAPATAN'} value={data.worth} />
+	</TableRow>
 
-	const addRow = (data) => {
-		return (
-			<TableRow key={data.name}>
-				<TableCell component={data.name === 'PENDAPATAN' ? 'th' : 'td'} scope="row">
-					{data.name}
-				</TableCell>
-				<DefaultFormattedCell value={data.weight} unit='kg' th={data.name === 'PENDAPATAN'}/>
-				<CurrencyFormattedCell th={data.name === 'PENDAPATAN'} value={data.worth} />
-			</TableRow>
-		)
-	}
+export default function SummaryTable({ dataset }) {
+
+	const rows = dataset.map(data => <Rows key={data.name} data={data} />)
+
 
 	return (
-		<TableContainer sx={{
-			mt: 2
-		}}>
-			<Table size="small" sx={{
-				'th': {
-					fontWeight: 'bold'
-				}, 'td, th': {
-					whiteSpace: 'nowrap'
-				}
-			}}>
+		<TableContainer sx={SX_TABLE_CONTAINER}>
+			<Table size="small" sx={SX_TABLE}>
 				<TableHead>
 					<TableRow>
 						<TableCell>TOTAL</TableCell>
@@ -43,7 +36,7 @@ export default function SummaryTable({dataset}) {
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{dataset.map(data => addRow(data))}
+					{rows}
 				</TableBody>
 			</Table>
 		</TableContainer>
