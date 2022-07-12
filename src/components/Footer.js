7b3f76fea8c2d014a2f01d5d03d1425173896d64
Zain Adam <zainadam.id@gmail.com>
2022-07-12 09:43:30 +0700

    fix: new tab on click company link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,19 @@ import { GALog } from "../helpers/firebaseClient";
 
 
 const SX = { mt: 4, mb: 4, textAlign: 'center' }
+const COMPANY_URL = "https://github.com/sensasi-apps"
+const ON_CLICK_LINK = (e) => GALog('click_company_page')
+const YEAR = new Date().getFullYear()
 
 function Footer() {
 	return (
 		<Box sx={SX} component='footer'>
 			<Typography variant="body2" color="text.secondary">
 				{'Copyright © '}
-				<Link color="inherit" href="https://github.com/sensasi-apps" onClick={(e) => GALog('click_company_page')}>
+				<Link color="inherit" href={COMPANY_URL} onClick={ON_CLICK_LINK} target="_blank">
 					Sensasi Apps
 				</Link>
-				{' ' + new Date().getFullYear() + '.'}
+				{' ' + YEAR + '.'}
 			</Typography>
 			<Typography variant='caption' color="text.secondary">v{process.env.REACT_APP_VERSION}</Typography>
 		</Box>
