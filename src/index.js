da78db42e74d4482de6773d43efc440a93bc2c07
Zain Adam <zainadam.id@gmail.com>
2022-07-21 11:41:45 +0700

    chore: remove analytics web viltal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { GALog } from "./helpers/firebaseClient";
 import {} from "./helpers/sentryClient";
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -14,18 +12,6 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-
-function sendToGoogleAnalytics({ name, delta, id }) {
-	GALog(name, {
-		event_category: 'Web Vitals',
-		event_label: id,
-		value: Math.round(name === 'CLS' ? delta * 1000 : delta),
-		non_interaction: true,
-	});
-}
-
-reportWebVitals(sendToGoogleAnalytics);
 
 
 serviceWorkerRegistration.register();
