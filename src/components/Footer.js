cd41d8e3edaf6378a0f7977f402c52a2b3307a62
Zain Adam <zainadam.id@gmail.com>
2023-05-04 01:03:08 +0700

    chore: reformat date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,9 +19,12 @@ function Footer() {
 				<Link color="inherit" href={COMPANY_URL} onClick={ON_CLICK_LINK} target="_blank">
 					Sensasi Apps
 				</Link>
-				{' ' + updatedAt.format('DD-MM-YYYY') + '.'}
+				{' ' + updatedAt.format('YYYY')}
 			</Typography>
-			<Typography variant='caption' color="text.secondary">v{process.env.REACT_APP_VERSION} ({updatedAt.format('L')})</Typography>
+			<Typography variant='caption' color="text.secondary">v{process.env.REACT_APP_VERSION}</Typography>
+			<Box>
+				<Typography variant='caption' color="text.secondary">{updatedAt.format('DD-MM-YYYY')}</Typography>
+			</Box>
 		</Box>
 	);
 }
