55d2e7907a16db5c2a8558e7611f14832d46c377
Zain Adam <zainadam.id@gmail.com>
2024-10-09 20:02:43 +0800

    Add Serwist

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,9 @@
-import type { ReactNode } from 'react'
+// vendors
+import { useEffect, type ReactNode } from 'react'
 import { createTheme, ThemeProvider } from '@mui/material'
+import { getSerwist } from 'virtual:serwist'
 import { red } from '@mui/material/colors'
+// local providers
 import { GlobalsProvider } from './components/globals-provider'
 import { FirebaseProvider } from './components/firebase-provider'
 
@@ -13,6 +16,24 @@ const THEME = createTheme({
 })
 
 export function Providers({ children }: { children: ReactNode }) {
+    useEffect(() => {
+        const loadSerwist = async () => {
+            if ('serviceWorker' in navigator) {
+                const serwist = await getSerwist()
+
+                serwist?.addEventListener('installed', () => {
+                    console.log('Serwist installed!')
+                })
+
+                void serwist?.register()
+            }
+        }
+
+        loadSerwist().catch(err => {
+            throw err
+        })
+    }, [])
+
     return (
         <ThemeProvider theme={THEME}>
             <FirebaseProvider>
