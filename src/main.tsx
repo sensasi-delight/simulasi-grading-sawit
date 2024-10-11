2f5d8a38a353aa5f3d362972adb674400664049e
Adam Akbar <zainadam.id@gmail.com>
2024-10-11 20:46:01 +0800

    Change `@sentry/browser` to `@sentry/react` (#27)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+import './instrument'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Providers } from './providers'
