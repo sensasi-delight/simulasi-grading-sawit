6ea0d6686e49254eb458293d275d26f1f8076a90
Adam Akbar <zainadam.id@gmail.com>
2024-10-11 19:37:36 +0800

    Static value of `appId` because GH secret not support ':' char (#26)

--- src/providers/components/firebase-provider.tsx
+++ src/providers/components/firebase-provider.tsx
@@ -19,7 +19,7 @@ const FIREBASE_APP = import.meta.env.PROD
           projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
           storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
           messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-          appId: import.meta.env.VITE_FIREBASE_APP_ID,
+          appId: '1:608340716838:web:4bcc8d7a239554e1eb65e7',
           measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
       })
     : null
