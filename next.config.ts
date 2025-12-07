e236689b4d71b5fff27c9c4c915f6e5d9c9294c0
Zain🍕 <zainadam.id@gmail.com>
2025-12-08 01:48:17 +0800

    Enables React Compiler for enhanced performance
    Integrates the React Compiler by enabling the `reactCompiler` flag in `next.config.ts` to leverage its benefits for optimizing React applications.
    
    Adds `babel-plugin-react-compiler` and `install` to development dependencies. Updates related Babel packages in the lock file to ensure compatibility and improve stability. Bumps the project version to reflect these changes.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     output: 'export',
     distDir: 'dist',
+    reactCompiler: true,
 
     experimental: {
         optimizePackageImports: ['@mui/material', '@mui/icons-material'],
